ca54ea794dde86360520b898d2e7d69e95fa3648
Cory Brunson <brunson@uchc.edu>
2016-07-10 08:13:47 -0400

    extend to multiple predicates

--- R/scale-by-predicate.r
+++ R/scale-by-predicate.r
@@ -2,16 +2,22 @@
 #' 
 #' @example inst/examples/scale-by-predicate.r
 #' @param data Data frame.
-#' @param predicate A logical expression involving variables in \code{data}.
-#' @return A binary matrix (formal context).
+#' @param predicates A logical expression involving variables in \code{data}, or
+#'   a list of such expressions.
+#' @return A binary numeric matrix (formal context) of \code{nrow(data)} rows 
+#'   and 1 column, whose entries are the truth values of \code{predicate} 
+#'   applied to the rows of \code{data}.
 #' @export
-scale_by_predicate <- function(data, predicate) {
-  # evaluate predicate
-  col <- as.numeric(eval(substitute(predicate), envir = data))
-
-  # return new column in matrix form
-  ctx <- as.matrix(col, ncol = 1)
+scale_by_predicate <- function(data, predicates) {
+  # evaluate predicate(s)
+  evals <- eval(substitute(predicates), envir = data)
+  # organize them as a data frame
+  ctx <- as.data.frame(evals)
   rownames(ctx) <- rownames(data)
-  colnames(ctx) <- as.expression(substitute(predicate))
+  colnames(ctx) <- if (!is.list(evals)) {
+    as.expression(substitute(predicates))
+  } else {
+    as.character(substitute(predicates))[-1]
+  }
   ctx
 }
--- inst/examples/scale-by-predicate.r
+++ inst/examples/scale-by-predicate.r
@@ -4,3 +4,9 @@ scale_by_predicate(mtcars, mpg > 20)
 
 # logical expressions involving multiple variables
 scale_by_predicate(mtcars, cyl > 4 & wt < 3)
+
+# multiple logical expressions
+scale_by_predicate(mtcars, list(cyl > 4, wt < 3, mpg > 20))
+
+# singleton list
+scale_by_predicate(mtcars, list(cyl == 6))
