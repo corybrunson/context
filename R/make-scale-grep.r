36b2b04a803f412a0afca6389b267468d7a3943f
corybrunson <jabrunso@vt.edu>
2016-07-10 14:20:39 -0400

    tidy make_scale_grep

--- R/make-scale-grep.r
+++ R/make-scale-grep.r
@@ -9,6 +9,9 @@
 #' @return A binary matrix (formal context).
 #' @export
 make_scale_grep <- function(objs, patterns, split) {
+  # each object should only appear once (in order of appearance)
+  objs <- unique(objs)
+  
   # use patterns if present; otherwise split
   if (missing(patterns)) {
     patterns <- unique(unlist(strsplit(objs, split = split)))
@@ -16,9 +19,6 @@ make_scale_grep <- function(objs, patterns, split) {
     if (!missing(split)) warning("'patterns' provided; ignoring 'split")
   }
 
-  # each object should only appear once (in order of appearance)
-  objs <- unique(objs)
-  
   # conceptual scale based on logical grep along patterns
   scale_mat <- sapply(patterns, grepl, x = objs)
   class(scale_mat) <- "numeric"
