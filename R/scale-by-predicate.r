143a41adc727684091c76619366b1d27dd1e3a1e
corybrunson <jabrunso@vt.edu>
2016-07-10 13:08:04 -0400

    allow named predicate lists, pass names to context columns

--- R/scale-by-predicate.r
+++ R/scale-by-predicate.r
@@ -3,21 +3,24 @@
 #' @example inst/examples/scale-by-predicate.r
 #' @param data Data frame.
 #' @param predicates A logical expression involving variables in \code{data}, or
-#'   a list of such expressions.
+#'   a list of such expressions. If a list, then the names of any named elements
+#'   will be used as column names in the resulting context.
 #' @return A binary numeric matrix (formal context) of \code{nrow(data)} rows 
 #'   and 1 column, whose entries are the truth values of \code{predicate} 
 #'   applied to the rows of \code{data}.
 #' @export
 scale_by_predicate <- function(data, predicates) {
-  # evaluate predicate(s)
-  evals <- eval(substitute(predicates), envir = data)
+  # substitute predicates
+  subs <- substitute(predicates)
+  # evaluate predicates
+  evals <- eval(subs, envir = data)
   # organize them as a data frame
   ctx <- as.data.frame(evals)
   rownames(ctx) <- rownames(data)
   colnames(ctx) <- if (!is.list(evals)) {
-    as.expression(substitute(predicates))
+    as.expression(subs)
   } else {
-    as.character(substitute(predicates))[-1]
+    ifelse(names(subs)[-1] != "", names(subs)[-1], as.character(subs)[-1])
   }
   ctx
 }
--- inst/examples/scale-by-predicate.r
+++ inst/examples/scale-by-predicate.r
@@ -10,3 +10,8 @@ scale_by_predicate(mtcars, list(cyl > 4, wt < 3, mpg > 20))
 
 # singleton list
 scale_by_predicate(mtcars, list(cyl == 6))
+
+# list with some predicates named
+scale_by_predicate(mtcars, list(`4-cyl` = cyl == 4,
+                                wt > 2,
+                                goodMPG = mpg > 20))
