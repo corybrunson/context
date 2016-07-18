fcc9286233d39a2833bf5bdf2c216bb44054a740
Cory Brunson <brunson@uchc.edu>
2016-07-18 10:04:22 -0400

    chi-squared binarization by overrepresentation only

--- R/binarize-matrix.r
+++ R/binarize-matrix.r
@@ -1,15 +1,17 @@
-#' Convert a frequency table (non-negative numeric matrix) to a matrix of the
-#' same dimensions containing at each position $(i,j)$ the value $1$ if the
-#' selected test statistic meets the provided cutoff threshold and $0$
-#' otherwise.
+#' Convert a frequency table (non-negative numeric matrix) to a matrix of the 
+#' same dimensions containing at each position $(i,j)$ the value $1$ if the 
+#' selected test statistic meets the provided cutoff threshold and $0$ 
+#' otherwise. In the case of a chi-squared test, an entry in the result is 1
+#' only if the corresponding entry in the input matrix was greater than
+#' expected.
 #' 
 #' @name binarize_matrix
 #' @param x Numeric non-negative matrix.
-#' @param stat Character string. The test statistic, either \code{chisq} or
+#' @param stat Character string. The test statistic, either \code{chisq} or 
 #'   \code{ratio}.
 #' @param thres Numeric. The cutoff threshold for the test statistic.
-#' @param test Function taking one argument \code{x} and giving logical output.
-#'   If provided, used to determine whether an entry is assigned the value $1$
+#' @param test Function taking one argument \code{x} and giving logical output. 
+#'   If provided, used to determine whether an entry is assigned the value $1$ 
 #'   (if returns \code{TRUE}) or $0$ (\code{FALSE}).
 #' @return Binary numeric matrix.
 #' @export
@@ -55,7 +57,15 @@ matrix_chisq <- function(x) {
     for(j in 1:ncol(mat_chisq)) {
       ct <- matrix(c(x[i, j], sum(x[-i, j]), sum(x[i, -j]), sum(x[-i, -j])),
                    nrow = 2)
-      mat_chisq[i, j] <- stats::chisq.test(ct)$statistic
+      # matrix of expected values, based on marginal frequencies
+      ct_exp <- as.matrix(rowSums(ct)) %*% t(colSums(ct)) / sum(ct)
+      # if ct[1, 1] is larger than expected, assign it the chi-square statistic;
+      # otherwise, assign it the value zero
+      mat_chisq[i, j] <- if (ct[1, 1] <= ct_exp[1, 1]) {
+        0
+      } else {
+        sum(((ct - ct_exp) ^ 2) / ct_exp)
+      }
     }
   }
   
